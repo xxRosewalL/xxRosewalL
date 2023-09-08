5a5d854143c3e9ca5284003dfc239e52abe0d07b
xxRosewalL <141931326+xxRosewalL@users.noreply.github.com>
2023-09-08 15:46:47 +0700

    Create entahlahpopup.js

---
+++ entahlahpopup.js
@@ -0,0 +1,6 @@
+function popupku() {
+  var body = document.querySelector("body");      
+  var pWm = document.createElement("a");
+  pWm.setAttribute("style", "text-decoration: none; color: white; opacity: .5; position: fixed; bottom : 10px; left: 10px");
+  body.appendChild(pWm);
+}
